9666c3f58c63a6c252954fc5d59f60fc79e53c04
Gabriel Melo <gabrielmelo1992@gmail.com>
2025-03-24 14:52:34 -0300

    pypi build

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ from distutils.core import setup
 scripts = glob.glob('bin/*')
 
 setup(
-  name='docx2txt',
-  packages=['docx2txt'],
-  version='0.8',
-  description='A pure python-based utility to extract text and images '
-              'from docx files.',
-  author='Ankush Shah',
-  author_email='ankush.shah.nitk@gmail.com',
-  url='https://github.com/ankushshah89/python-docx2txt',
-  download_url='https://github.com/ankushshah89/python-docx2txt/tarball/0.8',
-  keywords=['python', 'docx', 'text', 'images', 'extract'],
-  scripts=scripts,
-  classifiers=[],
+    name='docx2txt-setuptools-hotfix',
+    packages=['docx2txt'],
+    version='0.8.1',
+    description='Fork of docx2txt with a hotfix applied. A pure python-based utility to extract text and images from docx files.',
+    author='Gabriel Melo (forked from Ankush Shah)',
+    author_email='gabrielmelo1992@gmail.com',
+    url='https://github.com/gkmelo-dev/python-docx2txt',
+    download_url='https://github.com/gkmelo-dev/python-docx2txt/archive/refs/tags/0.8.0-hotfix1.tar.gz',
+    keywords=['python', 'docx', 'text', 'images', 'extract', 'hotfix'],
+    scripts=scripts,
+    classifiers=[],
+    license='MIT',
 )
